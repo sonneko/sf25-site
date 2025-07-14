40aed80d7fbfa9f773498467022ca8725582fd41
sonneko27 <sonneko27@gmail.com>
2025-07-14 16:20:44 +0000

    add [news_id] page

--- src/app/news/[news_id]/page.tsx
+++ src/app/news/[news_id]/page.tsx
@@ -1,3 +1,17 @@
-export default function EachNewsPage() {
-  return <>This is each news page.</>;
+interface Params {
+  news_id: string;
+}
+
+export async function generateStaticParams() {
+  return [{ news_id: 'iuu' }, { news_id: '2' }, { news_id: '3' }];
+}
+
+export default async function EachNewsPage({ params }: { params: Promise<Params> }) {
+  const newsId = (await params).news_id;
+  return (
+    <>
+      <div style={{ marginTop: '1000px' }} />
+      <p>This is each news page with id: {newsId}</p>
+    </>
+  );
 }
