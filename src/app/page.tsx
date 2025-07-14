21597a5b1d053eec03c12de218de022d8d5334af
sonneko27 <sonneko27@gmail.com>
2025-07-14 16:20:28 +0000

    add top page concrete element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import TopSlideShow from '../components/TopSlideShow/TopSlideShow';
+import CountDown from '../components/CountDown/CountDown';
+import News from '../components/News/News';
+
 export default function HomePage() {
-  return <>Hello World</>;
+
+  return (
+    <>
+      <main style={{ marginTop: '70px', position: 'relative', zIndex: 1 }}>
+        <TopSlideShow />
+        <CountDown />
+        <News />
+      </main>
+    </>
+  );
 }
