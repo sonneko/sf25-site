5b1e7324b7cf054e266b38468ae325b092c8f55a
Sonneko <152277125+sonneko@users.noreply.github.com>
2025-09-06 08:17:23 +0000

    update remove PannhirettoDownloadButton from Floating component

--- src/components/Floating/Floating.tsx
+++ src/components/Floating/Floating.tsx
@@ -1,9 +1,7 @@
-import PannhirettoDownloadButton from "../PannhirettoDownloadButton/PannhirettoDownloadButton";
-
 export default function Floating()  {
     return (
         <>
-            <PannhirettoDownloadButton />
+            
         </>
     )
 }
