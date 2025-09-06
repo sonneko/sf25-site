0f0b0e9f735b8579454d885e1133140314d50ae3
Sonneko <152277125+sonneko@users.noreply.github.com>
2025-09-06 08:18:39 +0000

    add stop using style sttribute and add infosection and access display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import TopSlideShow from '../components/TopSlideShow/TopSlideShow';
 import CountDown from '../components/CountDown/CountDown';
 import News from '../components/News/News';
+import { InfoSection } from '../components/InfoSection/InfoSection';
+
+import styles from "./page.module.scss";
+import { Access } from '../components/Access/Access';
 
 export default function HomePage() {
 
   return (
     <>
-      <main style={{ marginTop: '70px', position: 'relative', zIndex: 1 }}>
+      <main className={styles.container}>
         <TopSlideShow />
         <CountDown />
         <News />
+        <InfoSection title='SFとは？'>
+          SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />SFとは何か説明する。<br />
+        </InfoSection>
+        <InfoSection title='SF運営代表挨拶'>
+          SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。<br />SF運営代表のありがたいお言葉。
+        </InfoSection>
+        <Access />
       </main>
     </>
   );
