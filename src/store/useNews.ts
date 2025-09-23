a306dc1f4efefc72d5a4f976a72025f8e287f8d0
sonneko <sonneko27@gmail.com>
2025-09-23 16:02:37 +0900

    add stores

--- src/store/useNews.ts
+++ src/store/useNews.ts
@@ -1,42 +1,15 @@
 import { type IndexData } from '@/assets-types/Index';
 import { create } from 'zustand';
-
-interface DataStore<T> {
-    getAll(): T 
-}
+import type DataStore from '@/store/stores';
 
 interface NewsStore extends DataStore<IndexData["news"]> {
-  readonly news: IndexData['news'] | null;
-  getAll: () => IndexData['news'];
-  getUntileN: (limit: number) => IndexData['news'];
-};
-const useNewsStore = create<NewsStore>((set) => {
-    news: null,
-    
-})
-
-interface BlogStore extends DataStore<IndexData["blogs"]> {
-  readonly blogs: IndexData['blogs'] | null;
-  getAll: () => IndexData['blogs'];
-  getById: () => IndexData['blogs'];
+  data: IndexData['news'] | null;
+  setData: (data: IndexData['news']) => void;
 };
 
-interface TimeTableStore extends DataStore<IndexData["timeTable"]> {
-  readonly timeTable: IndexData['timeTable'] | null;
-  getAll: () => IndexData['timeTable'];
-  getById: () => IndexData['timeTable'];
-};
-
-interface LostItemStore extends DataStore<IndexData["lostItems"]> {
-  readonly lostItems: IndexData['lostItems'] | null;
-  getAll: () => IndexData['lostItems'];
-  getById: () => IndexData['lostItems'];
-};
-
-
-const useStore = create(set => ({
-  count: 0,
-  increment: () => set(state => ({ count: state.count + 1 })),
+const useNews =  create<NewsStore>((set) => ({
+    data: null,
+    setData: (data) => set(() => ({ data }))
 }));
 
-export default useStore;
+export default useNews;
