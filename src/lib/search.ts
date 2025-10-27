2217ce660d3d1fbb382005e1485ee7ec69557cf6
senpuki-b <163736161+senpuki-b@users.noreply.github.com>
2025-10-28 07:33:32 +0900

    update @ts-ignore to @ts-expect-error

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -14,25 +14,25 @@ function levenshteinDistance(str1: string, str2: string): number {
   const dp = Array.from({ length: len1 + 1 }, () => Array(len2 + 1).fill(0));
 
   // 初期化
-  // @ts-ignore
+  // @ts-expect-error
   for (let i = 0; i <= len1; i++) dp[i][0] = i;
-  // @ts-ignore
+  // @ts-expect-error
   for (let j = 0; j <= len2; j++) dp[0][j] = j;
 
   // DPテーブルを埋める
   for (let i = 1; i <= len1; i++) {
     for (let j = 1; j <= len2; j++) {
       if (str1[i - 1] === str2[j - 1]) {
-        // @ts-ignore
+        // @ts-expect-error
         dp[i][j] = dp[i - 1][j - 1]; 
       } else {
-        // @ts-ignore
+        // @ts-expect-error
         dp[i][j] = Math.min(
-          // @ts-ignore
+          // @ts-expect-error
           dp[i - 1][j] + 1, // 削除
-          // @ts-ignore
+          // @ts-expect-error
           dp[i][j - 1] + 1, // 挿入
-          // @ts-ignore
+          // @ts-expect-error
           dp[i - 1][j - 1] + 1 // 置換
         );
       }
@@ -40,7 +40,7 @@ function levenshteinDistance(str1: string, str2: string): number {
   }
 
   // 最終的な編集距離を返す
-  // @ts-ignore
+  // @ts-expect-error
   return dp[len1][len2];
 }
 
